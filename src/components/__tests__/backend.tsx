6e3f4d883c9ee9383c1053de42e2494022416729
Jesse Neumann <jesseneumann@gmail.com>
2019-07-24 15:17:45 -0600

    testing

--- src/components/__tests__/backend.tsx
+++ src/components/__tests__/backend.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { expect } from 'chai';
 import Backend from '../backend';
 
 it('renders without crashing', () => {
@@ -10,9 +11,13 @@ it('renders without crashing', () => {
 it('renders 5 db images', () => {
   // check and make sure there are imgs
   const wrapper = shallow(<Backend />);
-  const img1 =
-    '<img className="fadeInSlow" alt="tech-icon" style={styles.svg} src={mySqlImg} />';
-  expect(wrapper.contains(img1)).toEqual(true);
+  expect(wrapper.find('img')).to.have.lengthOf(5);
 });
 
-it('renders db description', () => {});
+it('renders db description', () => {
+  // make sure there is a h2 with db description
+  const wrapper = shallow(<Backend />);
+  const description =
+    'Experience with different databases means choosing the correct database for the most optimal solution.';
+  expect(wrapper.find('h2').text()).to.equal(description);
+});
